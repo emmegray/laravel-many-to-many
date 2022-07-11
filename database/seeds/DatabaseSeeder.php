d69e1c29b07fe9e00c3d8badc612380c0a8be8e2
Manuela Ienuso <43219535+emmegray@users.noreply.github.com>
2022-07-11 15:24:26 +0200

    First

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PostTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(UpdatePostsSeeder::class);
+        $this->call(TagsTableSeeder::class);
     }
 }
